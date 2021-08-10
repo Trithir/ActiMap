5d8e20970a128e23122c4cf3585ae85d4e0b1d12
Trithir <ericstevens10@gmail.com>
2021-08-10 16:31:58 -0700

    Added simple grid. Calendar dots. Working on orientation next

--- App.js
+++ App.js
@@ -4,19 +4,38 @@ import { StyleSheet, Text, View } from 'react-native';
 import Calen from './Components/Calendar';
 import TodoForm from './Components/todoForm';
 import TodoList from './Components/todoList';
+// import { Col, Row, Grid } from "react-native-grid-layout";
+// import { NativeBaseProvider, Box } from 'native-base';
+import { Col, Row, Grid } from "react-native-easy-grid";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Grid style={styles.container}>
+      <Row size={15}></Row>
+      <Row size={10}><Text>Emotional</Text></Row>
+      <Row size={10}><TodoForm/></Row>
+      <Row size={12}><Text>ALow</Text></Row>
+      <Row size={50}><Calen/></Row>
+    </Grid>
+    );
+    // <View style={styles.container}>
+
+      {/* <Grid>
+        <Row size={1}><Text>Emotional</Text></Row>
+        <Row size={1}> <TodoForm /></Row>
+        <Row size={1}> <Text>ALow</Text></Row>
+        <Row size={3}> <Calen /></Row>
+      </Grid> */}
+
+      {/* <StatusBar style="auto" />
       <Text>Emotional</Text>
-      {/* <TodoList /> */}
       <TodoForm />
-        {/* <MyComponent style={styles.map}/> */}
       <Text>ALow</Text>
-      <Calen />
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Calen /> */}
+      {/* <TodoList /> */}  
+      {/* <MyComponent style={styles.map}/> */}
+    // </View>
+  
 }
 
 const styles = StyleSheet.create({
