1f6f0c9b0f5436415e59c7a38903f02afe4a613c
Trithir <ericstevens10@gmail.com>
2021-08-11 16:53:25 -0700

    Trying to set up orientation

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import Calen from './Components/Calendar';
 import TodoForm from './Components/todoForm';
 import TodoList from './Components/todoList';
@@ -8,7 +7,87 @@ import TodoList from './Components/todoList';
 // import { NativeBaseProvider, Box } from 'native-base';
 import { Col, Row, Grid } from "react-native-easy-grid";
 
+// export default class App extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//         OrientationStatus : '',
+//         Height_Layout : '',
+//         Width_Layout : '',
+//     }
+//   }
+//   componentDidMount(){
+//     this.DetectOrientation();
+//   }
+//   DetectOrientation(){
+//     console.log(this.state.Width_Layout,this.state.Height_Layout)
+//     if(this.state.Width_Layout > this.state.Height_Layout)
+//     {
+//       // Write Your own code here, which you want to execute on Landscape Mode.
+
+//       this.setState({
+//         OrientationStatus : 'Landscape'
+//         });
+//     }
+//     else{
+
+//         this.setState({
+//         OrientationStatus : 'Portrait'
+//         });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.MainContainer} onLayout={(event) => this.setState({
+//         Width_Layout : event.nativeEvent.layout.width,
+//         Height_Layout : event.nativeEvent.layout.height
+//       }, ()=> this.DetectOrientation())}>
+      
+//       {this.state.OrientationStatus == "Landscape" ? console.log("landscape") 
+//       :
+//       <Grid style={styles.container}>
+//         <Row size={15}></Row>
+//         <Row size={10}><Text>Emotional</Text></Row>
+//         <Row size={10}><TodoForm/></Row>
+//         <Row size={12}><Text>ALow</Text></Row>
+//         <Row size={50}><Calen/></Row>
+//       </Grid>
+//       }
+
+//       </View>
+//   );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   MainContainer: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     margin: 10
+//   },
+//   TextStyle :{
+//     fontSize : 20,
+//     color : '#000'
+//   },
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   map: {
+//     width: 100,
+//     height: 75,
+//   }
+// });
+
+
+
 export default function App() {
+
+
   return (
     <Grid style={styles.container}>
       <Row size={15}></Row>
@@ -18,7 +97,7 @@ export default function App() {
       <Row size={50}><Calen/></Row>
     </Grid>
     );
-    // <View style={styles.container}>
+    <View style={styles.container}>
 
       {/* <Grid>
         <Row size={1}><Text>Emotional</Text></Row>
@@ -34,11 +113,21 @@ export default function App() {
       <Calen /> */}
       {/* <TodoList /> */}  
       {/* <MyComponent style={styles.map}/> */}
-    // </View>
+    </View>
   
 }
 
 const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10
+  },
+  TextStyle :{
+    fontSize : 20,
+    color : '#000'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
